95a1745b5a5ca82357aca012a4bf4d34789ae9cc
kendallli587 <kendallli587@gmail.com>
2022-07-22 09:50:35 -0600

    a

--- js/functions.js
+++ js/functions.js
@@ -11,10 +11,29 @@ function drawBoard() {
 
 function allSpaces() {
     let temp = [];
-    for (let x = 50; x <= 750; x += 100) {
-        for (let y = 50; y <= 750; y += 100) {
+    for (let y = 50; y <= 750; y += 100) {
+        for (let x = 50; x <= 750; x += 100) {
             temp.push({x: x, y: y});
+        
         }
     }
     return temp;
+}
+
+function initAliens() {
+    let temp = [];
+    for (let i = 0; i < 6; i++) {
+        temp.push(spaces[i]);
+    }
+    return temp;
+}
+
+function drawAliens(thisAlien) {
+    fill("green");
+    circle(thisAlien.x, thisAlien.y, 40, "fill");
+}
+
+function moveAliens(thisAlien) {
+    fill("green");
+    circle(thisAlien.x + 100, thisAlien.y, 40, "fill");
 }
--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
 
 // Global Variables
 let spaces = allSpaces();
+let aliens = initAliens();
+
+requestAnimationFrame(draw)
+
+function draw() {
+    background("white");
+    drawBoard();
+
+    for (let i = 0; i < aliens.length; i++) {
+        drawAliens(aliens[i]);
+        moveAliens(aliens[i]);
+    }
+
+
+    requestAnimationFrame(draw)
+}
